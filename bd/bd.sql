4ed142414ece83c4fd8335531ca97c940bd87d22
Abdieldixon22 <juanjosecondoripinto@gmail.com>
2023-05-02 11:10:25 -0500

    database

---
+++ bd/bd.sql
@@ -0,0 +1,9 @@
+create database grupo;
+use grupo;
+create or replace table employees(
+    id int auto_increment primary key,
+    nombres varchar(50) not null,
+    apellidos varchar(50) not null,
+    dni numeric(9) not null unique,
+    salario numeric(6,2)
+);
