78e41bdedbbb05490a785e8aeec8b2cd0eac6657
Abdieldixon22 <juanjosecondoripinto@gmail.com>
2023-05-02 19:01:25 -0500

    edit function implementation

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
-    <title>Proyecto Grupo 2</title>
+    <title>Proyecto Grupo</title>
 </head>
 <body>
 
