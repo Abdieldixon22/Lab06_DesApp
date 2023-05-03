e9f3259118851392dcf196fa9b2d41b39c249243
Abdieldixon22 <juanjosecondoripinto@gmail.com>
2023-05-02 22:17:30 -0500

    empty delete

--- php/delete.php
+++ php/delete.php
@@ -1,17 +1,3 @@
 <?php
-    include_once 'conexion.php';
-
-    $idEmployee = $_GET['codigo'];
-
-    $sentencia = $bd->prepare('DELETE FROM employees WHERE id=?');
-    $resultado = $sentencia->execute([$idEmployee]);
     
-    // Reactualización de id
-    $bd->query('ALTER TABLE employees AUTO_INCREMENT='.$idEmployee);
-
-    if ($resultado === TRUE){
-        header('Location: ..\index.php?mensaje=eliminado');
-    } else {
-        header('Location: ..\index.php?mensaje=error');
-    }
 ?>
